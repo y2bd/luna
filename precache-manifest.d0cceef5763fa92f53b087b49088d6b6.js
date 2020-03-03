b346c61709770406562859b48a1567b0536b9f5c
Jason Lo <github@jasonlo.email>
2020-03-03 14:13:42 -0800

    Updates

---
+++ precache-manifest.d0cceef5763fa92f53b087b49088d6b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1225e9325b0eecbb123f326a0d77b807",
+    "url": "/luna/index.html"
+  },
+  {
+    "revision": "48714d69624edc53ca56",
+    "url": "/luna/static/css/main.82965868.chunk.css"
+  },
+  {
+    "revision": "35da77165c95caed6017",
+    "url": "/luna/static/js/2.4fecef14.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/luna/static/js/2.4fecef14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48714d69624edc53ca56",
+    "url": "/luna/static/js/main.bce421df.chunk.js"
+  },
+  {
+    "revision": "a7cea39e3d539760654a",
+    "url": "/luna/static/js/runtime-main.0a86d2d4.js"
+  }
+]);
